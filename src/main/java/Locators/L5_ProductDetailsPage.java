7ec2704e21c3a33ef8dbc1788b8c07d2d97bc8a0
YoussefSewalm <eng.yousefsewalem@gmail.com>
2024-11-18 17:36:57 +0200

    Update L5_ProductDetailsPage.java

--- src/main/java/Locators/L5_ProductDetailsPage.java
+++ src/main/java/Locators/L5_ProductDetailsPage.java
@@ -8,8 +8,8 @@ public class L5_ProductDetailsPage {
 	//Text
 	public static By ProductNameInProductDetailsText=By.className("bgnone");
 	public static By ProductPriceInProductDetailsText=By.className("productfilneprice");
-    public static By ProductTotalPriceFromProductDetailsText=By.cssSelector("label[class=\"control-label\"]>span[class=\"total-price\"]");
-    public static By SelectSizeText=By.cssSelector("div[class=\"alert alert-error alert-danger\"]");
+        public static By ProductTotalPriceFromProductDetailsText=By.cssSelector("label[class=\"control-label\"]>span[class=\"total-price\"]");
+        public static By SelectSizeText=By.cssSelector("div[class=\"alert alert-error alert-danger\"]");
 	//Links
 	public static By AddProductToCartLink=By.cssSelector("a[class=\"cart\"]");
 	//FormField
