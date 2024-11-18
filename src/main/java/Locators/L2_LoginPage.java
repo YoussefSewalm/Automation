b2daa71d21b0211d6fc5cffeae416038cbe9fdc3
YoussefSewalm <eng.yousefsewalem@gmail.com>
2024-11-18 17:36:23 +0200

    Update L2_LoginPage.java

--- src/main/java/Locators/L2_LoginPage.java
+++ src/main/java/Locators/L2_LoginPage.java
@@ -3,10 +3,10 @@ package Locators;
 import org.openqa.selenium.By;
 
 public class L2_LoginPage {
-	//Text
-	public static By AccountLoginText=By.className("heading1");
-	public static By AccountLoggedInText=By.cssSelector("div[class=\"menu_text\"]");
-	public static By IncorrectLoginText=By.cssSelector("div[class=\"alert alert-error alert-danger\"]");
+	 //Text
+    public static By AccountLoginText=By.className("heading1");
+    public static By AccountLoggedInText=By.cssSelector("div[class=\"menu_text\"]");
+    public static By IncorrectLoginText=By.cssSelector("div[class=\"alert alert-error alert-danger\"]");
 	//FormField
     public static By LoginNameFormField=By.id("loginFrm_loginname");
     public static By LoginPasswordFormField=By.id("loginFrm_password");
