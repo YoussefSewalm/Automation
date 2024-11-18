be4a5cbe3593198934ce9bead04dfbcc4f7f0478
YoussefSewalm <eng.yousefsewalem@gmail.com>
2024-11-18 17:34:04 +0200

    Update Alerts.java

--- src/main/java/CoreElements/Alerts.java
+++ src/main/java/CoreElements/Alerts.java
@@ -10,13 +10,11 @@ import org.testng.Assert;
 import com.shaft.driver.SHAFT;
 
 public class Alerts {
-	//hena 3shan l alerts msh 3amla extend ll Elements class f lazm a3rf l driver tany w a3mlo Setter
 	
     public static SHAFT.GUI.WebDriver driver;
 
 	public void AlertAccept()
 	{
-		//Using FluentWait : hena l WebDriver 2ly 3mltlo Import bta3 l Org,Selenium msh l Shaft
 		Wait<WebDriver> wait=new FluentWait<>(driver.getDriver())
 				.withTimeout(Duration.ofSeconds(15))
 				.pollingEvery(Duration.ofMillis(300))
@@ -29,11 +27,6 @@ public class Alerts {
 		         	});
 		
 		driver.getDriver().switchTo().alert().accept();
-		//l Fluent Wait b2a by3ml eh??,,badelo wa2t timeout 3ady 10sec msln..w byfdl y3ml Polling kol 300Ms
-		//w y5osh fl Until w yshof hal ana 3malt switch to alert fe3ln wla la2 ya3ny hal fe3ln l alert
-		//geh wla lesa.. law geh hy3ml switch 3leh w y3ml return true w y5rog mn l loop deh aw l function deh
-		//w ykml 3ady w y3ml accept.....tb law mgash lesa hyrg3 tany ystna 300ms kman w b3d keda yro7 ll 
-		//Until tany w hakza		
 	}
 	public void AlertConfirmation(String ExpectedAlertMessage)
 	{
