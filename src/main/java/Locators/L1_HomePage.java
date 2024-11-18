fc5cf536c46b36f89a5730e59a6ef21f79fcf165
YoussefSewalm <eng.yousefsewalem@gmail.com>
2024-11-18 17:35:48 +0200

    Update L1_HomePage.java

--- src/main/java/Locators/L1_HomePage.java
+++ src/main/java/Locators/L1_HomePage.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.By;
 
 public class L1_HomePage {
 
-	//Buttons
+   //Buttons
     public static By SearchButton=By.cssSelector("div[class=\"button-in-search\"]");
-	//Links
-	public static By HeaderHomePageLink=By.xpath("(//a[@href=\"https://automationteststore.com/\"])[1]");
+   //Links
+    public static By HeaderHomePageLink=By.xpath("(//a[@href=\"https://automationteststore.com/\"])[1]");
     public static By CartLink=By.xpath("(//a[@href=\"https://automationteststore.com/index.php?rt=checkout/cart\"])[1]");
     public static By LoginOrRegisterLink=By.xpath("(//a[@href=\"https://automationteststore.com/index.php?rt=account/login\"])[1]");
     public static By CheckOutLink=By.xpath("(//a[@href=\"https://automationteststore.com/index.php?rt=checkout/shipping\"])[1]");
     public static By MyAccountLink=By.xpath("(//a[@class=\"top menu_account\"])[1]");
     public static By ApparelAndAccessoriesLink=By.cssSelector("a[href=\"https://automationteststore.com/index.php?rt=product/category&path=68\"]");
-	public static By HomePageLink=By.cssSelector("a[class=\"active menu_home\"]");
+    public static By HomePageLink=By.cssSelector("a[class=\"active menu_home\"]");
     //Home Drop Menu Links
     public static By HomeDropMenuCheckOutLink=By.xpath("(//a[@href=\"https://automationteststore.com/index.php?rt=checkout/shipping\"])[3]");
     public static By HomeDropMenuLoginLink=By.xpath("(//a[@href=\"https://automationteststore.com/index.php?rt=account/login\"])[3]");
@@ -38,13 +38,13 @@ public class L1_HomePage {
     public static By HomeDropMenuAccountDropMenu=By.xpath("(//a[@class=\"top menu_account\"])[2]");
     public static By MyAccountDropMenu=By.xpath("(//a[@class=\"top menu_account\"])[1]");
     public static By CartDropMenu=By.xpath("(//a[@href=\"https://automationteststore.com/index.php?rt=checkout/cart\"])[2]");
-	public static By HomePageDropMenu=By.cssSelector("a[class=\"active menu_home\"]");
-	public static By CurrencyDropMenu=By.xpath("(//a[@class=\"dropdown-toggle\"])[1]");
+    public static By HomePageDropMenu=By.cssSelector("a[class=\"active menu_home\"]");
+    public static By CurrencyDropMenu=By.xpath("(//a[@class=\"dropdown-toggle\"])[1]");
     public static By ApparelAndAccessoriesDropMenu=By.cssSelector("a[href=\"https://automationteststore.com/index.php?rt=product/category&path=68\"]");
-	//FormField
-	public static By SearchTextField=By.cssSelector("div[class=\"btn-group search-bar\"]>input");
-	//Text
-	public static By HomePageText=By.cssSelector("a[class=\"active menu_home\"]");
+    //FormField
+    public static By SearchTextField=By.cssSelector("div[class=\"btn-group search-bar\"]>input");
+    //Text
+    public static By HomePageText=By.cssSelector("a[class=\"active menu_home\"]");
 
 }
 
